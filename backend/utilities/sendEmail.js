8ed33cc878a4cf1ab669301bce590ddd3dd860b5
moatasem <motasem.yakhni@gmail.com>
2022-10-27 20:27:46 +0300

    Backend => create sendEmail function in utilities folder

--- backend/utilities/sendEmail.js
+++ backend/utilities/sendEmail.js
@@ -9,3 +9,33 @@ const PASSWORD = process.env.PASS;
 const APP_IMAGE_LOGO_STORAGE_PATH = process.env.APP_IMAGE_LOGO_STORAGE_PATH;
 const TOKEN_EXPIRY_TIME_IN_SECONDS = process.env.TOKEN_EXPIRY_TIME_IN_SECONDS;
 /**********************/
+
+const sendEmail = async (fullName, email, subject, text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: HOST,
+            service: SERVICE,
+            port: 587,
+            secure: true,
+            auth: {
+                user: USER,
+                pass: PASSWORD,
+            },
+        });
+
+        await transporter.sendMail({
+            from: 'CodeCam Team',
+            to: email,
+            subject: subject,
+            html: emailTemplate(fullName, text),
+            attachments: [{
+                filename: 'logo.png',
+                path: `${APP_IMAGE_LOGO_STORAGE_PATH}`,
+                cid: 'logo.png',
+            }],
+        });
+        console.log('Email is sent successfully');
+    } catch (error) {
+        console.log('error:', error.message);
+    }
+}
