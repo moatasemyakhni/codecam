2f5396b4f6642453076c5df5e323f8f72ecd4c66
moatasem <motasem.yakhni@gmail.com>
2022-10-27 18:27:09 +0300

    Backend => create photoExtensions allowed in conditional variables

--- backend/utilities/conditionalVariables.js
+++ backend/utilities/conditionalVariables.js
@@ -6,9 +6,12 @@ const minimumPasswordLength = 6;
 
 const allowedProgrammingLanguages = ['C', 'CPP14', 'CSHARP', 'JAVA8', 'JAVASCRIPT_NODE', 'PHP', 'PYTHON2', 'PYTHON3'];
 
+const photoExtensions = ['JPEG', 'PNG', 'JPG'];
+
 module.exports = {
     minimumNameLength,
     maximumNameLength,
     minimumPasswordLength,
     allowedProgrammingLanguages,
+    photoExtensions,
 }
