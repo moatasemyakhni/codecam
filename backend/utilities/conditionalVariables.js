486a511a4e147174ea4844ded7df32f4e22b31f4
moatasem <motasem.yakhni@gmail.com>
2022-10-27 18:04:07 +0300

    Backend => create allowed programming languages variable in conditional variables

--- backend/utilities/conditionalVariables.js
+++ backend/utilities/conditionalVariables.js
@@ -4,8 +4,11 @@ const maximumNameLength = 25;
 
 const minimumPasswordLength = 6;
 
+const allowedProgrammingLanguages = ['C', 'CPP14', 'CSHARP', 'JAVA8', 'JAVASCRIPT_NODE', 'PHP', 'PYTHON2', 'PYTHON3'];
+
 module.exports = {
     minimumNameLength,
     maximumNameLength,
     minimumPasswordLength,
+    allowedProgrammingLanguages,
 }
