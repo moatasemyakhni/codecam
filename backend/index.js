7ace8dcea55f3bdfa59f6e444e1a5efe3f5aa9ca
moatasem <motasem.yakhni@gmail.com>
2022-10-27 16:07:06 +0300

    Backend => create basic nodejs server

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+
+const PORT = process.env.PORT;
+
+
+
+const app = express();
+app.use(express.json({limit: '5mb'}));
+
+
+
+app.listen(PORT, () => console.log('Start Server'));
