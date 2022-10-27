19ceef049aaf565799780ef883cecfb4e0d16552
moatasem <motasem.yakhni@gmail.com>
2022-10-27 17:22:03 +0300

    Backend => User_Controller: create getUserByToken function

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
+
+// Personal Information
 const createJWTToken = (user) => {
     const token = jwt.sign(
         {
@@ -17,3 +19,19 @@ const createJWTToken = (user) => {
 
     return token;
 } 
+
+const getUserByToken = async (req, res) => {
+    const token = req.headers.authorization;
+
+    try {
+        const decoded = jwt.verify(token.split(' ')[1], process.env.USER_ACCESS_TOKEN);
+        const user = await User.findOne({email: decoded.email}).select({email: 0, createdAt: 0, updatedAt: 0});
+        if(!user) {
+            throw {message: "User not found"};
+        }
+
+        res.status(200).send({error: false, user: user});
+    } catch (error) {
+        res.status(404).send({error: true, message: error.message});
+    }
+}
