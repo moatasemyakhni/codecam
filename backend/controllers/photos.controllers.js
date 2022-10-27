2f85c0fe588ca32083ced7edb54dfc65196810af
moatasem <motasem.yakhni@gmail.com>
2022-10-27 18:22:35 +0300

    Backend => Photo_Controller: create getPhotosByUserId function

--- backend/controllers/photos.controllers.js
+++ backend/controllers/photos.controllers.js
@@ -3,3 +3,18 @@ const Photo = require('../models/Photo');
 const fs = require('fs');
 
 const {getUserById} = require('./users.controller');
+
+
+const getPhotosByUserId = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        if(!userId) {
+            throw {message: 'User id is required'};
+        }
+        const photos = await Photo.find({userId: userId}).sort([['updatedAt', 'desc']]);
+
+        res.status(200).send({error: false, photos: photos});
+    } catch (error) {
+        res.status(400).send({error: true, message: error.message});
+    }
+}
