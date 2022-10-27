c19d69d6508160d5f586c223082c32351569b19c
moatasem <motasem.yakhni@gmail.com>
2022-10-27 22:59:26 +0300

    Backend => connect routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('./config/db.config');
-
+const users = require('./routes/user.routes');
+const photos = require('./routes/photo.routes');
 const express = require('express');
 
 
@@ -11,6 +12,11 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json({limit: '5mb'}));
 
+//users main route
+app.use('/users', users);
+
+//photos main route
+app.use('/photos', photos);
 
 
 app.listen(PORT, () => console.log('Start Server'));
