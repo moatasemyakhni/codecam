b9e83a2bc423b3ade72dbe9fa3af45b0545230f1
moatasem <motasem.yakhni@gmail.com>
2022-10-27 17:30:27 +0300

    Backend => User_Controller: import bcrypt for login and signup functions

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 /**********************/
 // .env variables
