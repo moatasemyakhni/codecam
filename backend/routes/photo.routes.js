5a5823f801e14f6290254eb143db806e90b28fd9
moatasem <motasem.yakhni@gmail.com>
2022-10-27 22:53:44 +0300

    Backend => Photo_Routes: create route for deleting a photo

--- backend/routes/photo.routes.js
+++ backend/routes/photo.routes.js
@@ -6,12 +6,14 @@ const {
     getPhotoById,
     getPhotosByUserId,
     editPhotoById,
+    deletePhoto,
 } = require('../controllers/photos.controllers');
 
 const router = express.Router();
 router.get('/photo/:photoId', userAuthMiddleware, getPhotoById);
-router.post('/', userAuthMiddleware, savePhoto);
 router.get('/:userId', userAuthMiddleware, getPhotosByUserId);
+router.post('/', userAuthMiddleware, savePhoto);
 router.patch('/photo/:photoId', userAuthMiddleware, editPhotoById);
+router.delete('/:photoId', userAuthMiddleware, deletePhoto);
 
 module.exports = router;
