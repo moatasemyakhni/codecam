033d76d0b3d79c409be4f06f3d6c398bc39c681b
moatasem <motasem.yakhni@gmail.com>
2022-10-31 18:02:19 +0200

    Backend => User_Routes: move functions that do not require auth to auth.routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const userAuthMiddleware = require('../middlewares/user.middleware');
-
 const {
-    signup,
-    sendEmailToUser,
-    login,
-    changePassword,
     editProfile,
     editFullName,
     codeOutput,
@@ -14,14 +9,7 @@ const {
 
 const router = express.Router();
 
-// no auth needed
 
-router.post('/', signup);
-router.post('/login', login);
-router.post('/reset-password', sendEmailToUser);
-router.post('/change-password', changePassword);
-
-// needs authorization
 router.get('/', userAuthMiddleware, getUserByToken);
 router.post('/run', userAuthMiddleware, codeOutput);
 router.patch('/edit/photo/:userId', userAuthMiddleware, editProfile);
