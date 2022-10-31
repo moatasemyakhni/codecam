afde4f6b72994da0744341ad4af1e65f74043022
moatasem <motasem.yakhni@gmail.com>
2022-10-31 18:07:18 +0200

    Backend => Photo_Routes: all functions inside the file requires authorization so the middleware is moved to index.js

--- backend/routes/photo.routes.js
+++ backend/routes/photo.routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const userAuthMiddleware = require('../middlewares/user.middleware');
 
 const {
     savePhoto,
@@ -10,10 +9,10 @@ const {
 } = require('../controllers/photos.controllers');
 
 const router = express.Router();
-router.get('/photo/:photoId', userAuthMiddleware, getPhotoById);
-router.get('/:userId', userAuthMiddleware, getPhotosByUserId);
-router.post('/', userAuthMiddleware, savePhoto);
-router.patch('/photo/:photoId', userAuthMiddleware, editPhotoById);
-router.delete('/:photoId', userAuthMiddleware, deletePhoto);
+router.get('/photo/:photoId', getPhotoById);
+router.get('/:userId', getPhotosByUserId);
+router.post('/', savePhoto);
+router.patch('/photo/:photoId', editPhotoById);
+router.delete('/:photoId', deletePhoto);
 
 module.exports = router;
