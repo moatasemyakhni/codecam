1211af3b1ec05f7253a7217b111f28062b5c7dd6
moatasem <motasem.yakhni@gmail.com>
2022-10-31 18:06:02 +0200

    Backend => import userAuth middleware to index.js to add it on user and photo routes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require('./config/db.config');
 const users = require('./routes/user.routes');
 const photos = require('./routes/photo.routes');
 const express = require('express');
-
+const userAuthMiddleware = require('./middlewares/user.middleware');
 
 const PORT = process.env.PORT;
 
@@ -13,10 +13,10 @@ const app = express();
 app.use(express.json({limit: '5mb'}));
 
 //users main route
-app.use('/users', users);
+app.use('/users', userAuthMiddleware, users);
 
 //photos main route
-app.use('/photos', photos);
+app.use('/photos', userAuthMiddleware, photos);
 
 
 app.listen(PORT, () => console.log('Start Server'));
