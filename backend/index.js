d574313ca07ceb913c780884d26bc297ae72a754
moatasem <motasem.yakhni@gmail.com>
2022-11-02 18:42:35 +0200

    Backend => import auth routes to the main server index page

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,24 @@
 require('dotenv').config();
 require('./config/db.config');
+
 const users = require('./routes/user.routes');
 const photos = require('./routes/photo.routes');
+const auth = require('./routes/auth.routes');
 const express = require('express');
 const userAuthMiddleware = require('./middlewares/user.middleware');
-
 const PORT = process.env.PORT;
 
 
 
 const app = express();
-app.use(express.json({limit: '5mb'}));
 
+app.use(express.json({limit: '5mb'}));
 //users main route
 app.use('/users', userAuthMiddleware, users);
-
 //photos main route
 app.use('/photos', userAuthMiddleware, photos);
 
+// accounts settings(login, signup, change password...)
+app.use('/', auth);
 
 app.listen(PORT, () => console.log('Start Server'));
