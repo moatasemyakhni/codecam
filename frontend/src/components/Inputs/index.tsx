8bd546f1ff3677b2d54de148fa7a2fa1252882c0
moatasem <motasem.yakhni@gmail.com>
2022-11-06 21:09:02 +0200

    Frontend => Input_Component: keyboard dismiss when input is out of focus

--- frontend/src/components/Inputs/index.tsx
+++ frontend/src/components/Inputs/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import React, { FC } from 'react'
 import { colors } from '../../constants/palette'
 import {styles} from './styles'
@@ -12,23 +12,30 @@ interface InputProps {
 }
 
 const Input: FC<InputProps> = (props) => {
+  
   return (
-    <View>
-        <Text style={[styles.label, props.error? styles.errorMessage: styles.labelText]}>
-            {
-                !props.error?(
-                    props.label
-                ): (
-                    props.errorMessage
-                )
-            }
-        </Text>
-      <TextInput
-        style={ [styles.input, props.error? styles.inputError: styles.inputErrorFree] }
-        placeholder={props.placeholder}
-        placeholderTextColor={colors.darkMode.white}
-      />
-    </View>
+    
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={{ flex: 1 }}>
+          <Text style={[styles.label, props.error? styles.errorMessage: styles.labelText]}>
+              {
+                  !props.error?(
+                      props.label
+                  ): (
+                      props.errorMessage
+                  )
+              }
+          </Text>
+            <View>
+              <TextInput
+                style={ [styles.input, props.error? styles.inputError: styles.inputErrorFree] }
+                placeholder={props.placeholder}
+                placeholderTextColor={colors.darkMode.white}
+                keyboardType='default'
+              />
+            </View>
+        </View>
+      </TouchableWithoutFeedback>
   )
 }
 
