cb8b6f1f30f4dff2e363cbd7bae7163890760104
moatasem <motasem.yakhni@gmail.com>
2022-11-06 20:05:53 +0200

    Frontend => Input_Component: create input structure

---
+++ frontend/src/components/Inputs/index.tsx
@@ -0,0 +1,42 @@
+import { View, Text, TextInput, FC } from 'react-native'
+import React from 'react'
+import { colors } from '../../constants/palette'
+import {styles} from './styles'
+
+interface InputProps {
+    error?: boolean,
+    errorMessage?: string,
+    placeholder?: string,
+    label?: string,
+
+}
+
+const Input: FC<InputProps> = (props) => {
+  return (
+    <View>
+        <Text style={[styles.label, props.error? styles.errorMessage: styles.labelText]}>
+            {
+                !props.error?(
+                    props.label
+                ): (
+                    props.errorMessage
+                )
+            }
+        </Text>
+      <TextInput
+        style={ [styles.input, props.error? styles.inputError: styles.inputErrorFree] }
+        placeholder={props.placeholder}
+        placeholderTextColor={colors.darkMode.white}
+      />
+    </View>
+  )
+}
+
+Input.defaultProps = {
+    placeholder: "Write Here...",
+    label: "label",
+    errorMessage: "something went wrong",
+    error: false,
+}
+
+export default Input
