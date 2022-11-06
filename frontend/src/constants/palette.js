da38657ef622b14da268ff6436bcc64ce67fd16e
moatasem <motasem.yakhni@gmail.com>
2022-11-06 14:46:58 +0200

    Frontend => create palette.js file that inlcude constant colors to be used

---
+++ frontend/src/constants/palette.js
@@ -0,0 +1,16 @@
+export const colors = {
+    darkMode: {
+        primary: '#8293ee',
+        background: '#21262d',
+        white: '#f5f5f5',
+        green: '#39af7b',
+        blue: '#71a0ff',
+        red: '#ff5d5d',
+        transparent: 'rgba(255, 255, 255, 0.2)',
+    },
+}
+
+
+export default {
+    colors,
+}
