995b41bb4af29cbab44f33f49fa4410230a1d215
moatasem <motasem.yakhni@gmail.com>
2022-11-06 19:44:52 +0200

    Frontend => Button_Component: add conidtion to enable/disable button and change button bg-color

--- frontend/src/components/Buttons/FullWidthButton.tsx
+++ frontend/src/components/Buttons/FullWidthButton.tsx
@@ -6,23 +6,36 @@ import {styles} from './styles'
 interface FullWidthButtonProps {
     title?: string, 
     onPress?: () => void,
+    BGRed?: boolean,
+    BGBlue?: boolean,
+    BGGreen?: boolean,
+    BGPrimary?: boolean,
+    enabled?: boolean
 }
 
 const FullWidthButton: FC<FullWidthButtonProps> = (props) => {
+// an enabled prop will be passed once all fields become non-empty
   return (
-    <TouchableOpacity onPress={ props.onPress }>
-        <View style={styles.container}>
-            <Text style={styles.fullWidth}>
-                {props.title}
-            </Text>
-        </View>
-    </TouchableOpacity>
+    <View style={{ flex: 1, marginHorizontal: 10, }}>    
+        <TouchableOpacity disabled={!props.enabled} onPress={ props.onPress }>
+            <View style={ [styles.btn , props.BGRed? styles.btnBGRed: props.BGBlue? styles.btnBGBlue : props.BGGreen? styles.btnBGGreen: styles.btnBGPrimary, !props.enabled? styles.disabled:''] }>
+                <Text style={styles.btnText}>
+                    {props.title}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    </View>
   )
 }
 
 FullWidthButton.defaultProps = {
     onPress: () => {},
     title: 'Confirm',
+    BGRed: false,
+    BGBlue: false,
+    BGGreen: false,
+    BGPrimary: true,
+    enabled: false,
 }
 
 export default FullWidthButton
