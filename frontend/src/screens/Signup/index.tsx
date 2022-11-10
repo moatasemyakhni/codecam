c88de025f52cbe1b00a2511ac64dd5a1ca8c78d5
moatasem <motasem.yakhni@gmail.com>
2022-11-10 13:56:25 +0200

    Frontend => Signup_Screen: clean structure

--- frontend/src/screens/Signup/index.tsx
+++ frontend/src/screens/Signup/index.tsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { View, KeyboardAvoidingView, ScrollView, Platform } from 'react-native'
-import Input from '../../components/Inputs'
-import FullWidthButton from '../../components/Buttons/FullWidthButton'
-import { styles } from './styles'
+import React from 'react';
+import { View, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
+import Input from '../../components/Inputs';
+import FullWidthButton from '../../components/Buttons/FullWidthButton';
+import { styles } from './styles';
+
+
 const Signup = () => {
 
     return (
-    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} enabled   keyboardVerticalOffset={100} style={styles.signupContainer}>
+    <KeyboardAvoidingView 
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+      enabled   
+      keyboardVerticalOffset={100} 
+      style={styles.signupContainer}
+    >
       <ScrollView>
           <View style={styles.form}>
             <Input label='Email' placeholder='Email...' />
             <Input inputGap label='Fullname' placeholder='Fullname...' />
             <Input isPassword label='Password' placeholder='Password...' />
             <Input isPassword inputGap label='Confirm Password' placeholder='Confirm Password...' />
-            <FullWidthButton BGPrimary title='signup' />
-            
+            <FullWidthButton BGPrimary title='signup' />  
           </View>
+
       </ScrollView>
     </KeyboardAvoidingView>
     )
 }
-export default Signup
+export default Signup;
