7386b85e660606ed73a8ebca78cc361ae52c1aae
moatasem <motasem.yakhni@gmail.com>
2022-11-13 19:14:45 +0200

    Frontend => Header_Screen: style screen

---
+++ frontend/src/screens/Header/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+import { colors } from '../../constants/palette';
+
+export const styles = StyleSheet.create({
+    container: { 
+        width: '100%',
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: 'center', 
+    },
+    title: { 
+        flex: 1, 
+        textAlign: 'center', 
+        fontSize: 22, 
+        fontWeight:'bold', 
+        color: colors.white,
+    },
+    image: { 
+        backgroundColor: colors.white,
+        width: 36,
+        height: 36,  
+        borderRadius: 20,
+        resizeMode: 'contain', 
+    },
+});
