07745a530754c7d9b47536361b3aef019b929b62
moatasem <motasem.yakhni@gmail.com>
2022-11-13 16:05:33 +0200

    Frontend => create allowedProgrammingLanguage array in utilities file to specify allowed programming languages

--- frontend/src/constants/utilities.js
+++ frontend/src/constants/utilities.js
@@ -4,3 +4,14 @@ export const getExtenstionFromFilePath = (path) => {
     const imageExtension = imageExtensionArr[imageExtensionArr.length - 1];
     return imageExtension;
 }
+
+export const allowedProgrammingLanguages = [
+    {label: 'C', value: 'C'}, 
+    {label: 'C++', value: 'CPP14'}, 
+    {label: 'C#', value: 'CSHARP'},
+    {label: 'JAVA', value: 'JAVA8'},
+    {label: 'JAVASCRIPT', value: 'JAVASCRIPT_NODE'},
+    {label: 'PHP', value: 'PHP'},
+    {label: 'PYTHON2', value: 'PYTHON2'},
+    {label: 'PYTHON3', value: 'PYTHON3'},
+];
