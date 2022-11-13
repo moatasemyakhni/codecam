0589036c52067789ecce04a2047e632090b35660
moatasem <motasem.yakhni@gmail.com>
2022-11-13 22:37:46 +0200

    Frontend => change order of items in allowedLanguages at utilities file

--- frontend/src/constants/utilities.js
+++ frontend/src/constants/utilities.js
@@ -6,12 +6,12 @@ export const getExtensionFromFilePath = (path) => {
 }
 
 export const allowedProgrammingLanguages = [
+    {label: 'JAVASCRIPT', value: 'JAVASCRIPT_NODE'},
+    {label: 'PYTHON2', value: 'PYTHON2'},
+    {label: 'PYTHON3', value: 'PYTHON3'},
     {label: 'C', value: 'C'}, 
     {label: 'C++', value: 'CPP14'}, 
     {label: 'C#', value: 'CSHARP'},
     {label: 'JAVA', value: 'JAVA8'},
-    {label: 'JAVASCRIPT', value: 'JAVASCRIPT_NODE'},
     {label: 'PHP', value: 'PHP'},
-    {label: 'PYTHON2', value: 'PYTHON2'},
-    {label: 'PYTHON3', value: 'PYTHON3'},
 ];
