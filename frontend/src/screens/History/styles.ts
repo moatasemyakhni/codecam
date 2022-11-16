2e311558bab14fc3c96d6604a4737382370c0887
moatasem <motasem.yakhni@gmail.com>
2022-11-16 16:35:54 +0200

    Frontend => History_Screen: style the screen

---
+++ frontend/src/screens/History/styles.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import { colors } from '../../constants/palette';
+
+
+
+export const styles = StyleSheet.create({
+    container: { 
+        backgroundColor: colors.darkMode.background,
+        flex: 1,
+      },
+});
