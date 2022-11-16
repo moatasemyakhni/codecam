c58b185e4718ab2591fdb81fae4683ab7529c897
moatasem <motasem.yakhni@gmail.com>
2022-11-16 10:05:30 +0200

    Frontend => Header_Screen: handle header that does not include a profile icon

--- frontend/src/screens/Header/index.tsx
+++ frontend/src/screens/Header/index.tsx
@@ -1,13 +1,14 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React, { FC } from 'react'
 import { styles } from './styles'
-
+import { StackNavigationProp } from '@react-navigation/stack';
 
 interface HeaderProps {
   imageSrc?: string,
   extension?: string,
   title?: string,
-  navigation?: any,
+  navigation?: StackNavigationProp<any>
+  includeIcon?: boolean
 }
 
 const Header: FC<HeaderProps> = (props) => {
@@ -15,27 +16,39 @@ const Header: FC<HeaderProps> = (props) => {
     <View style={styles.container}>
 
         <Text style={styles.title}>{props.title}</Text>
-        <TouchableOpacity onPress={() => {
-          props.navigation.navigate('Setting')
-      }
-        }>
         {
-        props.imageSrc?
-        <Image
-            source={require('../../../assets/images/icons/default-profile-sm.png')}
-        />
-        :
-        <Image
-            source={{ 
-              uri: `data:image/${props.extension};base64,${props.imageSrc}`
-            }}
-            style={styles.image}
-        />
+        props.includeIcon?
+          <TouchableOpacity 
+            onPress={
+              () => {
+              props.navigation.navigate('Setting')
+              }
+            }
+          >
+            {
+              !props.imageSrc?
+              <Image
+                  source={require('../../../assets/images/icons/default-profile-sm.png')}
+              />
+              :
+              <Image
+                  source={{ 
+                    uri: `data:image/${props.extension};base64,${props.imageSrc}`
+                  }}
+                  style={[styles.image, styles.smSize]}
+              />
+            }
+          </TouchableOpacity>
+    :
+          <Text style={styles.smSize}></Text>
     }
-    </TouchableOpacity>
     </View>
     
   )
+}
+
+Header.defaultProps = {
+  includeIcon: true,
 }
 
 export default Header
