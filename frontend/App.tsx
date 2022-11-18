21fb916166269dcaeda6bf4c877f5e87f44ae919
moatasem <motasem.yakhni@gmail.com>
2022-11-18 16:24:58 +0200

    Frontend => wrap main App with redux Provider

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import StackSwitcher from './src/navigation/StackSwitcher';
 
-export default function App() {
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import { RootSiblingParent } from 'react-native-root-siblings';
+
+
+const App = () => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    
+    <Provider store={store}>
+      <RootSiblingParent>
+          <StackSwitcher /> 
+      </RootSiblingParent>
+    </Provider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
