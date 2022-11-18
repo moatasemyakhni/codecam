61eb3fac2bd4262c134cb80ec7936de694824ba8
moatasem <motasem.yakhni@gmail.com>
2022-11-18 17:40:30 +0200

    Frontend => Setting_Screen: make props more readable

--- frontend/src/screens/Setting/index.tsx
+++ frontend/src/screens/Setting/index.tsx
@@ -81,11 +81,41 @@ export const Setting = (props) => {
       </View>
       </ScrollView>
 
-      <Prompt bgColor={colors.green} color={colors.green} title='Share App' description={'Do you want to move to messages?'} label='Share' onSuccessMessage={'move to messages'} setVisiblePrompt={setVisibleShare} visiblePrompt={visibleShare} onAction={handleShare} />
+      <Prompt 
+        bgColor={colors.green} 
+        color={colors.green} 
+        title='Share App' 
+        description={'Do you want to move to messages?'} 
+        label='Share' 
+        onSuccessMessage={'move to messages'} 
+        setVisiblePrompt={setVisibleShare} 
+        visiblePrompt={visibleShare} 
+        onAction={handleShare} 
+      />
 
-      <Prompt bgColor={colors.blue} color={colors.blue} title='About Us' description={'Do you want to move to LinkedIn?'} label='Linkedin' onSuccessMessage={'move to LinkedIn'} setVisiblePrompt={setVisibleAbout} visiblePrompt={visibleAbout} onAction={handleAboutUs}/>
+      <Prompt 
+        bgColor={colors.blue} 
+        color={colors.blue} 
+        title='About Us' 
+        description={'Do you want to move to LinkedIn?'} 
+        label='Linkedin' 
+        onSuccessMessage={'move to LinkedIn'} 
+        setVisiblePrompt={setVisibleAbout}
+        visiblePrompt={visibleAbout} 
+        onAction={handleAboutUs}
+      />
 
-      <Prompt bgColor={colors.red} color={colors.red} title='Logout' description={'Do you want to Logout?'} label='Logout' onSuccessMessage={'Logout Successfully'} setVisiblePrompt={setVisibleLogout} visiblePrompt={visibleLogout} onAction={handleLogout} />
+      <Prompt 
+        bgColor={colors.red} 
+        color={colors.red} 
+        title='Logout' 
+        description={'Do you want to Logout?'} 
+        label='Logout'
+        onSuccessMessage={'Logout Successfully'} 
+        setVisiblePrompt={setVisibleLogout}
+        visiblePrompt={visibleLogout} 
+        onAction={handleLogout} 
+      />
     </View>
   )
 }
