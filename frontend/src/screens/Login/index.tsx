4a10a204ea374c343d022be351052f4ae7253670
moatasem <motasem.yakhni@gmail.com>
2022-11-18 16:34:03 +0200

    Frontend => Login_Screen: initialize necessary useStates

--- frontend/src/screens/Login/index.tsx
+++ frontend/src/screens/Login/index.tsx
@@ -1,39 +1,88 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, KeyboardAvoidingView, ScrollView, Platform } from 'react-native'
-import LogoXL from '../../../assets/images/logos/LogoXL'
-import Input from '../../components/Inputs'
-import FullWidthButton from '../../components/Buttons/FullWidthButton'
-import { styles } from './styles'
+import Input from '../../components/Inputs';
+import React, {useState, useEffect} from 'react';
+import LogoXL from '../../../assets/images/logos/LogoXL';
+import FullWidthButton from '../../components/Buttons/FullWidthButton';
+
+import { 
+  View, 
+  Text, 
+  Platform,
+  ScrollView, 
+  TouchableOpacity, 
+  KeyboardAvoidingView, 
+} from 'react-native';
+import { styles } from './styles';
+import { store } from '../../redux/store';
+import { login } from '../../api/auth/authApi';
+import { getUserInfo } from '../../api/user/userApi';
+import { emailFormat } from '../../constants/utilities';
+import { updateUserProfile } from '../../redux/slices/userSlice';
 
 
 const Login = ({navigation}) => {
-    const login = () => {
-      navigation.navigate('Camera');
-    }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [emailMessage, setEmailMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    setEnabled([email, password].every(Boolean));
+  }, [email, password])
+
+  
+
+
     return (
-    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} enabled style={styles.loginContainer}>
-      <ScrollView>
-        <View>
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+        enabled 
+        style={styles.loginContainer}
+      >
+        <ScrollView>
+          <View>
 
-          <View style={styles.logoWrapper}>
-              <LogoXL />
-          </View>
-      
-          <View style={styles.form}>
-            <Input label='Email' placeholder='Email...' />
-            <Input isPassword inputGap label='Password' placeholder='Password...' />
-            <FullWidthButton enabled BGPrimary title='Login' onPress={login} />
-            <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
-                <Text style={[styles.links, styles.linkTopPadding]}>Signup</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Forgot Password')}>
-                <Text style={styles.links}>Forgot Password</Text>
-            </TouchableOpacity>
-          </View>
+            <View style={styles.logoWrapper}>
+                <LogoXL />
+            </View>
+        
+            <View style={styles.form}>
+              <Input 
+                error={emailError} 
+                setError={setEmailError}
+                errorMessage={emailMessage}  
+                setMessage={setEmailMessage}
+                val={email} 
+                setVal={setEmail} 
+                label='Email' 
+                placeholder='Email...' 
+              />
+              <Input 
+                val={password} 
+                setError={setPasswordError}
+                error={passwordError}
+                errorMessage={passwordMessage}
+                setVal={setPassword} 
+                setMessage={setPasswordMessage}
+                isPassword 
+                inputGap 
+                label='Password' 
+                placeholder='Password...' 
+              />
+              <FullWidthButton enabled={enabled} BGPrimary title='Login' onPress={loginUser} />
+              <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+                  <Text style={[styles.links, styles.linkTopPadding]}>Signup</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.navigate('Forgot Password')}>
+                  <Text style={styles.links}>Forgot Password</Text>
+              </TouchableOpacity>
+            </View>
 
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     )
 }
 export default Login
