01db3a9971435c91181fb4339c6adf5db21b80ca
moatasem <motasem.yakhni@gmail.com>
2022-11-18 15:45:43 +0200

    Frontend => Api_Photo: prepare getAllPhotosForUser api

---
+++ frontend/src/api/photo/photoApi.ts
@@ -0,0 +1,30 @@
+import { apiManager } from "../ApiManager";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+const photosBaseUrl = '/photos';
+const config = {
+    headers: {
+        Authorization: '',
+    },
+};
+const getToken = async () => await AsyncStorage.getItem('token');
+
+
+interface getAllPhotosForUserInterface {
+    (
+        userId: string,
+    ): Promise<any>
+}
+
+export const getAllPhotosForUser: getAllPhotosForUserInterface = async (userId) => {
+    try {
+        const token = await getToken();
+        config.headers.Authorization = `Bearer ${token}`;
+        const response = await apiManager.get(`${photosBaseUrl}/${userId}`, config);
+        return response.data;
+    } catch (error) {
+        return error.hasOwnProperty('response')? error.response.data : {...error, error: true};
+    }
+}
+
