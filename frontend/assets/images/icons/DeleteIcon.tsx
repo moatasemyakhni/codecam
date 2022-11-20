d5abb9fa8966443dd6915a76823f63f330e2baf0
moatasem <motasem.yakhni@gmail.com>
2022-11-20 20:29:47 +0200

    Frontend => color prop could be passed to DeleteIcon component

--- frontend/assets/images/icons/DeleteIcon.tsx
+++ frontend/assets/images/icons/DeleteIcon.tsx
@@ -1,10 +1,10 @@
 import * as React from "react"
 import Svg, { Path } from "react-native-svg"
 
-const DeleteIcon = () => (
+const DeleteIcon = ({color}) => (
   <Svg width={18} height={22} fill="none">
     <Path
-      fill="#8293EE"
+      fill={color? color : "#8293EE"}
       d="M2 19.167A2.34 2.34 0 0 0 4.333 21.5h9.334A2.34 2.34 0 0 0 16 19.167v-14H2v14zm15.167-17.5h-4.084L11.917.5H6.083L4.917 1.667H.833V4h16.334V1.667z"
     />
   </Svg>
