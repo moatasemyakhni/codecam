532c33ffc76241d541dfa21def504b0285d57128
moatasem <motasem.yakhni@gmail.com>
2022-11-20 08:37:33 +0200

    Frontend => import themeReducer to store in redux

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,9 +1,11 @@
 import userReducer from './slices/userSlice';
+import themeReducer from './slices/themeSlice';
 
 import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        ui: themeReducer,
     }
 });
