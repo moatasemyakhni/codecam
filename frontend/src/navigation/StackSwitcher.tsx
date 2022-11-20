008ebf0a4baf24e35b0da95c8dd2b104dec037f9
moatasem <motasem.yakhni@gmail.com>
2022-11-20 20:26:12 +0200

    Frontend => store user preferred theme as soon as app is opened n switch Navigation

--- frontend/src/navigation/StackSwitcher.tsx
+++ frontend/src/navigation/StackSwitcher.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AuthStack from './AuthStack';
 import MainStack from './MainStack';
 
-import { useSelector } from 'react-redux'
+import { store } from '../redux/store';
+import { useSelector } from 'react-redux';
+import { useColorScheme } from 'react-native';
+import { toggleTheme } from '../redux/slices/themeSlice';
 import { NavigationContainer } from "@react-navigation/native";
+
 
 
 const StackSwitcher = () => {
     const user = useSelector((state) => state?.user);
+    const currentTheme = useColorScheme();
 
+    useEffect(() => {
+        store.dispatch(toggleTheme(currentTheme))
+    }, [])
     return user?.userProfile ? <NavigationContainer><MainStack /></NavigationContainer> : <AuthStack /> 
 }
 
