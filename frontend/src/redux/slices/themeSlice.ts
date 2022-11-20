a5bdabb28e74a13cbfe41124b122e6a645fa581a
moatasem <motasem.yakhni@gmail.com>
2022-11-20 08:33:58 +0200

    Frontend => create slice sppecific for theme in redux

---
+++ frontend/src/redux/slices/themeSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+interface themeInterface {
+    theme: 'dark' | 'light',
+}
+
+const initialState: themeInterface = {
+    theme: 'dark',
+}
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState,
+    reducers: {
+        toggleTheme(state, action) {
+            state.theme = action.payload;
+        }
+    },
+});
+
+export const {
+    toggleTheme,
+
+} = themeSlice.actions;
+
+export default themeSlice.reducer;
