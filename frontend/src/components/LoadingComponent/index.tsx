f1f74282537962e97c21b0ada6c577be547320a2
moatasem <motasem.yakhni@gmail.com>
2022-11-24 22:33:15 +0200

    Frontend => Loading_Component: implement laoding indicator

--- frontend/src/components/LoadingComponent/index.tsx
+++ frontend/src/components/LoadingComponent/index.tsx
@@ -1,18 +1,59 @@
-import { View, Image } from 'react-native';
-import React from 'react';
-import {styles} from './style';
+import React, { useEffect, useState, FC } from 'react';
+import CircularProgress from 'react-native-circular-progress-indicator';
 
-const LoadingComponent = () => {
+import {styles} from './style';
+import { View } from 'react-native';
+import { colors } from '../../constants/palette';
+
+interface LoadingComponentInterface {
+  endOfProgress?: boolean,
+  smallLoadingIcon?: boolean,
+  title?: string,
+}
+
+const LoadingComponent: FC<LoadingComponentInterface> = ({ endOfProgress, smallLoadingIcon, title}) => {
+
+  const [value, setValue] = useState(97);
+  const [duration, setDuration] = useState(10000);
+
+  useEffect(() => {
+    if(endOfProgress) {
+      setValue(100);
+      setDuration(0);
+    }
+  }, [endOfProgress]);
+
   return (
-    <View style={styles.container}>
-      <Image
-      style={styles.image} 
-        source={
-            require('../../../assets/images/loading.gif')
+    <View style={[
+      styles.container,
+      smallLoadingIcon?
+        styles.containerPositionForSm
+      :
+        styles.containerPositionForLg
+    ]}>
+
+      <CircularProgress
+        radius={
+          smallLoadingIcon?
+            30
+          :
+            150
         }
+        value={value}
+        valueSuffix={'%'}
+        activeStrokeColor={colors.primary}
+        inActiveStrokeOpacity={0.2}
+        duration={duration}
+        title={title}
       />
     </View>
-  )
+  );
+}
+
+LoadingComponent.defaultProps = {
+  endOfProgress: false,
+  smallLoadingIcon: false,
+  title: '',
 }
 
 export default LoadingComponent
